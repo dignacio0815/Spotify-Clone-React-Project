02d8b417fad4f55ce62b23939e43c2f5808ca17f
Denize Ignacio <denizeignacio@gmail.com>
2022-11-23 11:36:12 -0800

    feat: implement Sidebar component

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -23,7 +23,7 @@ const Discover = () => {
         </h2>
         <select
           onChange={() => {
-            console.log("i was clicked!");
+
           }}
           value=""
           className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
